6d0ad4f6b945dab47419cce81bf9f0e489550edb
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2019-11-17 21:47:26 +0100

    setting up test and models and routes

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+/* eslint-disable no-console */
 // imports
 const express = require('express');
 const { Client } = require('pg');
+
 const app = express();
 
 // connection
-const db = "postgres://weezykonlocal:root@localhost/teamwork";
+const db = 'postgres://weezykonlocal:root@localhost/teamwork';
 const client = new Client({
-    connectionString: db
+  connectionString: db,
 });
-client.connect();
+client.connect().then(() => {
+  // eslint-disable-next-line no-console
+  console.log('Successfully connected to Postgres SQL!');
+})
+  .catch((error) => {
+    console.log('Unable to connect to Postgres SQL!');
+    console.error(error);
+  });
 
 // extracts the JSON object from the request
 const bodyParser = require('body-parser');
 
 // cors
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
 });
 
 // Using body parser
