7cccc2816c9ef5e4e8a0ea078ea28757110c1df1
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2019-11-27 18:21:36 +0100

    setting up security

--- routes/like.js
+++ routes/like.js
@@ -2,13 +2,20 @@
 // imports
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 
 const verify = require('./../middleware/verifyemployee');
 
 // Controllers
 const postCtrl = require('../controllers/posts');
 
+// rate limit
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 3,
+});
+
 // routes
-router.post('/', verify, postCtrl.likePost);
+router.post('/', [postLimiter, verify], postCtrl.likePost);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,21 @@
 // imports
 const express = require('express');
 const router = express.Router();
+const rateLimit = require('express-rate-limit');
 
 const verify = require('../middleware/verifyemployee');
 
 // Controllers
 const userCtrl = require('../controllers/user');
 
+// rate limit
+const postLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 2,
+});
+
 // admin routes
-router.get('/users', verify, userCtrl.getUsers);
-router.post('/auth/login', userCtrl.loginUser);
+router.get('/users', [postLimiter, verify], userCtrl.getUsers);
+router.post('/auth/login', postLimiter, userCtrl.loginUser);
 
 module.exports = router;
