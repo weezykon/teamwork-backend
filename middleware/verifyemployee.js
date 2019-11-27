b142518e4fd8d324b54f25028d2f35be9439e6ff
Akinbode Akinlolu Weezykon <weezykon@gmail.com>
2019-11-27 15:29:27 +0100

    fixed broken links

--- middleware/verifyemployee.js
+++ middleware/verifyemployee.js
@@ -3,16 +3,20 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const token = req.header('Authorization').replace('Bearer ', '');
-  if (!token) return res.status(401).send({ status: 'error', message: 'Access Denied' });
-  try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
-    if (verified.role === 'admin') {
+  if (req.header('Authorization')) {
+    const token = req.header('Authorization').replace('Bearer ', '');
+    if (!token) return res.status(401).send({ status: 'error', message: 'Access Denied' });
+    try {
+      const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.user = verified;
+      if (verified.role === 'admin') {
+        return res.status(400).send({ status: 'error', message: 'Token Invalid' });
+      }
+      next();
+    } catch (error) {
       return res.status(400).send({ status: 'error', message: 'Token Invalid' });
     }
-    next();
-  } catch (error) {
-    return res.status(400).send({ status: 'error', message: 'Token Invalid' });
+  } else {
+    return res.status(400).send({ status: 'error', message: 'You do not have access to this platform' });
   }
 };
